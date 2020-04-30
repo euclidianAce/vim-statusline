80b108d98042646de54c020ae639809b160e65ae
Corey Williamson <nerd1001001@gmail.com>
2020-04-30 18:17:23 -0500

    Added guicolors

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -19,8 +19,8 @@ end
 
 local function setup(t, key, tab)
 	cmd(string.format(
-	"hi StatusLineColor%s ctermfg=%d ctermbg=%d",
-	tab[1]:gsub(" ",""), tab[2], tab[3]
+	"hi StatusLineColor%s ctermfg=%d ctermbg=%d guifg=#%06x guibg=#%06x",
+	tab[1]:gsub(" ",""), tab[2], tab[3], tab[4], tab[5]
 	))
 	rawset(t, key, {
 		modeKey = tab[key],
@@ -37,14 +37,14 @@ local modes = setmetatable({}, {
 	end,
 	__newindex = setup
 })
-modes["n"] = {"Normal", 25, 87}
-modes["i"] = {"Insert", 28, 10}
-modes["R"] = {"Replace", 124, 204}
-modes["v"] = {"Visual", 58, 221}
-modes["V"] = {"Visual Line", 58, 221}
-modes[""] = {"Visual Block", 58, 221}
-modes["c"] = {"Command", 53, 134}
-modes["t"] = {"Terminal", 0, 185}
+modes["n"] = {"Normal", 25, 87, 0x005FAF, 0x5FFFFF}
+modes["i"] = {"Insert", 28, 10, 0x008700, 0x00FF00}
+modes["R"] = {"Replace", 124, 204, 0xAF0000, 0xff5f87}
+modes["v"] = {"Visual", 58, 221, 0x5F5F00, 0xFFD75F}
+modes["V"] = {"Visual Line", 58, 221, 0x5F5F00, 0xFFD75F}
+modes[""] = {"Visual Block", 58, 221, 0x5F5F00, 0xFFD75F}
+modes["c"] = {"Command", 53, 134, 0x5F005F, 0xAF5Fd7}
+modes["t"] = {"Terminal", 0, 185, 0x000000, 0xD7D75F}
 
 local function mode()
 	return modes[eval("mode()")]
@@ -52,7 +52,7 @@ end
 local fmt = table.concat{
 	"%s %s ", -- Highlighed mode name
 	"%%#BufferNumber# %%n ", -- buffer number
-	"%%#FileName# %%f %%y%%r%%h%%w%%m ", -- filename, extension, modification flag
+	"%%#FileName# %%.30f %%y%%r%%h%%w%%m ", -- filename, extension, modification flag
 	"%s", -- highlighted bar to show active window
 	"%%=", -- fill
 	"%%#LineNumber# %%l/%%L:%%c %%3p%%%%  ", -- line number, columns, percent through file
